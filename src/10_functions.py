573e634f64ca76c3496c8d5dbbd5af4a86ea2df7
Roger Caballero <rogercp1234@yahoo.com>
2019-09-03 15:24:33 -0700

    finished up to scopes file

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,15 @@
 
 # YOUR CODE HERE
 
+def if_true(num):
+	if num % 2 == 0:
+		return True
+	else:
+		return False
+
+print(if_true(5))
+print(if_true(4))
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +18,13 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+def if_true_from_keyboard(num):
+	if num % 2 == 0:
+		print("Even!")
+	else:
+		print("Odd!")
+
+if_true_from_keyboard(num)
+
 
